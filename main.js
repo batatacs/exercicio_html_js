509fac36589bfea8ac9fc2769b1b8effef565414
anderson camilo <anderson_cs345@hotmail.com>
2023-01-09 21:42:31 -0300

    exercicio html com js

---
+++ main.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('#from-control')
+const campo1 = document.querySelector('#campoA')
+const campo2 = document.querySelector('#campoB')
+const campoResultado = document.querySelector('#campoResult')
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log(campo1.value);
+    console.log(campo2.value);
+
+    if(campo2.value > campo1.value){
+        campoResultado.style.display = "flex"
+        campoResultado.style.justifyContent = "center"
+        campoResultado.style.backgroundColor = "rgb(12, 149, 12)"
+        campoResultado.innerHTML = `Formulário Válido`
+        
+    }else if (campo2.value < campo1.value) {
+        campoResultado.style.display = "flex"
+        campoResultado.style.justifyContent = "center"
+        campoResultado.style.backgroundColor = "red"
+        campoResultado.innerHTML = `Formulário Inválido`
+    }
+})
